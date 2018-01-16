b57c47270b0363ccce3c66081f1b0367258dcd1b
marusama <maru.sama@gmail.com>
2018-01-16 13:52:02 +0300

    Working version, code cleanup.

--- cyclicbarrier.go
+++ cyclicbarrier.go
@@ -20,9 +20,10 @@ type CyclicBarrier interface {
 	Reset()
 }
 
+// round
 type round struct {
-	count  int
-	waitCh chan struct{}
+	count  int           // count of goroutines for this roundtrip
+	waitCh chan struct{} // wait channel for this roundtrip
 }
 
 // cyclicBarrier impl CyclicBarrier intf
@@ -32,13 +33,6 @@ type cyclicBarrier struct {
 
 	lock  sync.Mutex
 	round *round
-	// count  int
-	// waitCh chan struct{}
-
-	// waiters int32
-
-	// bufCh   chan struct{}
-	// inReset int32
 }
 
 // New initializes a new instance of the CyclicBarrier, specifying the number of parties.
@@ -46,16 +40,12 @@ func New(parties int) CyclicBarrier {
 	if parties <= 0 {
 		panic("parties must be positive number")
 	}
-	//waitCh := make(chan struct{})
-	//bufCh := make(chan struct{}, parties-1)
 	return &cyclicBarrier{
 		parties: parties,
 		lock:    sync.Mutex{},
 		round: &round{
 			waitCh: make(chan struct{}),
 		},
-
-		//bufCh: bufCh,
 	}
 }
 
@@ -75,44 +65,6 @@ func NewWithAction(parties int, barrierAction func()) CyclicBarrier {
 	}
 }
 
-// func (b *cyclicBarrier) Await(ctx context.Context) error {
-// 	var ctxDoneCh <-chan struct{}
-// 	if ctx != nil {
-// 		ctxDoneCh = ctx.Done()
-// 	}
-
-// 	select {
-// 	case b.bufCh <- struct{}{}:
-// 		select {
-// 		case <-b.waitCh:
-// 		case <-ctxDoneCh:
-// 			return ctx.Err()
-// 		}
-// 	case <-ctxDoneCh:
-// 		b.Reset()
-// 		return ctx.Err()
-// 	default:
-// 		if b.barrierAction != nil {
-// 			b.barrierAction()
-// 		}
-// 		b.Reset()
-// 	}
-// 	return nil
-// }
-
-// func (b *cyclicBarrier) Reset() {
-// 	b.lock.Lock()
-// 	defer b.lock.Unlock()
-
-// 	for i := 0; i < b.parties-1; i++ {
-// 		<-b.bufCh
-// 	}
-
-// 	close(b.waitCh)
-// 	b.waitCh = make(chan struct{})
-// }
-
-//func (b *cyclicBarrier) Await(ctx context.Context, num int) error {
 func (b *cyclicBarrier) Await(ctx context.Context) error {
 
 	var ctxDoneCh <-chan struct{}
@@ -130,45 +82,23 @@ func (b *cyclicBarrier) Await(ctx context.Context) error {
 
 	b.lock.Unlock()
 
-	//println(num, "count++", count)
-
-	// decrement count
-	// decrementFunc := func() {
-	// 	b.lock.Lock()
-	// 	b.count--
-	// 	//println(num, "count--", b.count)
-	// 	b.lock.Unlock()
-	// }
-
 	if count > b.parties {
 		panic("CyclicBarrier.Await is called more than total parties count times")
 	}
 
 	if count < b.parties {
-		//println(num, "wait")
-
-		//atomic.AddInt32(&b.waiters, 1)
-
-		// wait other parties
 		select {
 		case <-waitCh:
-			//decrementFunc()
-			//atomic.AddInt32(&b.waiters, -1)
 			return nil
 		case <-ctxDoneCh:
-			//decrementFunc()
-			//atomic.AddInt32(&b.waiters, -1)
 			return ctx.Err()
 		}
 	} else {
-		//println(num, "reset")
 		// we are last, run the barrier action and break the barrier
 		if b.barrierAction != nil {
 			b.barrierAction()
 		}
-		//decrementFunc()
 		b.Reset()
-
 		return nil
 	}
 }
@@ -176,9 +106,10 @@ func (b *cyclicBarrier) Await(ctx context.Context) error {
 func (b *cyclicBarrier) Reset() {
 	b.lock.Lock()
 
-	//close(b.waitCh)
-	//b.waitCh = make(chan struct{})
+	// broadcast to pass waiting goroutines
 	close(b.round.waitCh)
+
+	// create new round
 	b.round = &round{
 		waitCh: make(chan struct{}),
 	}
